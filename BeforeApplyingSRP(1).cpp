02eee996a613ff2cf9a53d53b5505b8df59a8d2c
Anasss885 <62183487+Anasss885@users.noreply.github.com>
2023-05-01 07:17:40 +0200

    Create BeforeApplyingSRP(1).cpp

---
+++ BeforeApplyingSRP(1).cpp
@@ -0,0 +1,12 @@
+class User {
+public:
+    void login() {
+        cout << "User logged in" << endl;
+    }
+    void logout() {
+        cout << "User logged out" << endl;
+    }
+    void printUserInfo(string username) {
+        cout << "Username: " << username << endl;
+    }
+};
